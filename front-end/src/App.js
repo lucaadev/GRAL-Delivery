1c44e34ada6d9f6c4eb68f65e2f34a647daa9000
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-08-05 12:47:50 -0300

    fix: redirect route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
-import Home from './Pages/Home';
+// import Home from './Pages/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Home /> } />
+      <Route path="/" element={ <Navigate replace to="/login" /> } />
       <Route path="/login" element={ <Login /> } />
     </Routes>
   );
