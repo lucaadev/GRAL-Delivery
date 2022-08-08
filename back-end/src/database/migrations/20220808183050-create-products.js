d3bac25fcbce9c04b4a89c0ee35f13e928ca6246
Guilherme <guilhermehallmann@gmail.com>
2022-08-08 19:01:52 -0300

    fix: changing product price to decimal

--- back-end/src/database/migrations/20220808183050-create-products.js
+++ back-end/src/database/migrations/20220808183050-create-products.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       price: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.DECIMAL(4,2)
       },
       url_image: {
         allowNull: false,
