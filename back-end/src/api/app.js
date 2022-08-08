3c4f750759a02b4f625fa174b8499e923abf9d96
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-08-08 16:44:45 -0300

    feat: route get/products

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const loginRoute = require('../routes/login.route');
 const registerRoute = require('../routes/register.route');
+const productsRoute = require('../routes/products.route');
 const errorHandler = require('../middlewares/errorHandler');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
+app.use('/products', productsRoute);
 app.use(errorHandler);
 
 // app.get('/coffee', (_req, res) => res.status(418).end());
---
+++ back-end/src/database/models/Product.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true
+    },
+    price: {
+      allowNull: false,
+      type: DataTypes.NUMBER
+    },
+    url_image: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }}, {
+    timestamps: false,
+    tableName: 'products'
+  });
+  return Product;
+};
