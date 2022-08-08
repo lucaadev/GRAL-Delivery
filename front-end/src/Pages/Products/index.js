c5e4498d338a655a8ac9c40f9a071468daf8017e
Guilherme <guilhermehallmann@gmail.com>
2022-08-08 17:30:36 -0300

    feat: mapping products

--- front-end/src/Pages/Products/index.js
+++ front-end/src/Pages/Products/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Navbar';
 import Card from '../../components/ProductCard';
 import CartBtn from '../../components/CartBtn';
+import axiosInstance from '../../utils/axiosInstance';
 
 function Products() {
+  const [products, setProducts] = useState([]);
+  const getAllProducts = async () => {
+    try {
+      const allProducts = await axiosInstance.get('/products');
+      setProducts(allProducts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => getAllProducts());
   return (
     <section className="main-products">
       <Header />
       <section className="main-products-cards">
-        <Card id="1" title="teste" price={ 10 } image="teste" />
+        { products.map((product, i) => (
+          <Card
+            key={ i }
+            id={ product.id }
+            title={ product.name }
+            price={ product.price }
+            image={ product.url_image }
+          />
+        ))}
       </section>
       <CartBtn price={ 10 } />
     </section>
