04f130ccf4f1cea8e16f181578b958038f54ea45
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-08-09 13:22:47 -0300

    fix: validateAuth | set config headers - token

--- back-end/src/middlewares/validateAuth.js
+++ back-end/src/middlewares/validateAuth.js
@@ -1,22 +1,27 @@
 const jwt = require('jsonwebtoken');
-const { configJWT } = require('../utils/generateToken');
+const { readFileSync } = require('fs');
 const errorThrow = require('../utils/errorThrow');
 require('dotenv').config();
 
-const secretKey = process.env.JWT_SECRET || 'secret_key';
+const secretKey = readFileSync('jwt.evaluation.key', 'utf8');
 const messageUnauthorized = errorThrow(401, 'Token Not Found');
 const messageInvalidToken = errorThrow(401, 'Expired Or Invalid Token');
 
 const validateAuth = (req, _res, next) => {
-    // const { token } = req.headers.Authorization;
-    console.log('Back', req.headers);
-    if (!token) next(messageUnauthorized);
-
-    jwt.verify(token, secretKey, configJWT.algorithm, (error, userInfo) => {
-        if (error) next(messageInvalidToken);
-        req.userInfo = userInfo;
-    });    
-    next();
-};
-
+    const token = req.headers.authorization;
+    // console.log(req.headers.authorization);
+    if (!token || !secretKey) {
+        next(messageUnauthorized);
+    }  
+    try {
+      const data = jwt.verify(token, secretKey);
+      req.body = {
+        ...req.body,
+        userInfoToken: data,
+      };
+      next();
+    } catch (error) {
+      next(messageInvalidToken);
+    }
+  };
 module.exports = validateAuth;
