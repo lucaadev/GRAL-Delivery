93b7114378c9d29160948f218c715b901d996d9c
Guilherme <guilhermehallmann@gmail.com>
2022-08-10 17:13:00 -0300

    fix: missing return in delivery form component

--- front-end/src/components/DetailsDelivery.js
+++ front-end/src/components/DetailsDelivery.js
@@ -2,45 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function DetailsDelivery({ sellerName }) {
-  <section className="delivery-details">
-    <form>
-      <label htmlFor="vendedor-responsavel">
-        P.Vendedor Responsável:
-        { sellerName }
-        {' '}
+  return (
+    <section className="delivery-details">
+      <form>
+        <label htmlFor="vendedor-responsavel">
+          P.Vendedor Responsável:
+          { sellerName }
+          {' '}
 
-        <select data-testid="customer_checkout__select-seller">
-          <option value="vendedor">Vendedora 1</option>
-          <option value="vendedor">Vendedora 2</option>
-        </select>
-      </label>
+          <select data-testid="customer_checkout__select-seller">
+            <option value="vendedor">Vendedora 1</option>
+            <option value="vendedor">Vendedora 2</option>
+          </select>
+        </label>
 
-      <label htmlFor="endereco">
-        Endereço:
-        <input
-          data-testid="customer_checkout__input-address"
-          type="text"
-          id="endereco"
-          name="endereco"
-          required
-          minLength="4"
-          maxLength="100"
-          size="10"
-        />
-      </label>
+        <label htmlFor="endereco">
+          Endereço:
+          <input
+            data-testid="customer_checkout__input-address"
+            type="text"
+            id="endereco"
+            name="endereco"
+            required
+            minLength="4"
+            maxLength="100"
+            size="10"
+          />
+        </label>
 
-      <label htmlFor="numero">
-        Número:
-        <input
-          data-testid="customer_checkout__input-addressNumber"
-          type="number"
-          id="numero"
-          name="numero"
-          max="100"
-        />
-      </label>
-    </form>
-  </section>;
+        <label htmlFor="numero">
+          Número:
+          <input
+            data-testid="customer_checkout__input-addressNumber"
+            type="number"
+            id="numero"
+            name="numero"
+            max="100"
+          />
+        </label>
+      </form>
+    </section>
+  );
 }
 
 DetailsDelivery.propTypes = {
