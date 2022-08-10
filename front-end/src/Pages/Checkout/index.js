0c083fbd0136ef38097c51f69f1f91b53c26845a
Guilherme <guilhermehallmann@gmail.com>
2022-08-10 17:23:08 -0300

    fix: trying to fix error expected 2,2 received 2.2 table checkout price

--- front-end/src/Pages/Checkout/index.js
+++ front-end/src/Pages/Checkout/index.js
@@ -16,18 +16,22 @@ function Checkout() {
       <span>Finalizar pedido</span>
       <section>
         {
-          cart.map((item, i) => (
-            <TableRow
-              key={ i }
-              index={ i }
-              id={ item.id }
-              title={ item.title }
-              quantity={ item.quantity }
-              price={ item.price }
-              subTotal={ Number((item.quantity * item.price).toFixed(2)) }
-            />
-          ))
+          cart.map((item, i) => {
+            const priceFormat = `${item.price}`.replace('.', ',');
+            return (
+              <TableRow
+                key={ i }
+                index={ i }
+                id={ item.id }
+                title={ item.title }
+                quantity={ item.quantity }
+                price={ priceFormat }
+                subTotal={ (item.quantity * item.price).toFixed(2).replace('.', ',') }
+              />
+            );
+          })
         }
+
         <section>
           Total: R$
           <span data-testid="customer_checkout__element-order-total-price">
