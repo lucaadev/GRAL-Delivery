48dcb8b1e7bd8dcadcd16e967135f942b15c9b34
Rosalia Padoin Oliveira <koxelinho@icloud.com>
2022-08-10 15:33:40 -0300

    feat: refactor Controller

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -17,6 +17,4 @@ app.use('/products', productsRoute);
 app.use('/sales', salesRoute);
 app.use(errorHandler);
 
-// app.get('/coffee', (_req, res) => res.status(418).end());
-
 module.exports = app;
--- back-end/src/controller/productsController.js
+++ back-end/src/controller/productsController.js
@@ -1,6 +1,6 @@
 const productsService = require('../service/productsService');
 
-const getAllProducts = async (req, res, next) => {
+const getAllProducts = async (_req, res, next) => {
   try {
       const allProducts = await productsService.getAllProducts();
       return res.status(200).json(allProducts);
