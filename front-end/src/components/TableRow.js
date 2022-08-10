e6fca24bd351fe1704ee082fd91ae1006ccee820
Guilherme <guilhermehallmann@gmail.com>
2022-08-10 16:08:11 -0300

    feat: creating table rows and data test ids for tableRow component

--- front-end/src/components/TableRow.js
+++ front-end/src/components/TableRow.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function TableRow({ title, quantity, price, totalPrice }) {
-  <h1> Oi</h1>;
+function TableRow({ index, id, title, quantity, price, subTotal }) {
+  <table className="products-table">
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Descrição</th>
+        <th>Quantidade</th>
+        <th>Valor unitário</th>
+        <th>Sub-total</th>
+        <th>Remover item</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr key={ id }>
+        <td data-testid={ `customer_checkout__element-order-table-item-number-${index}` }>
+          { index + 1 }
+        </td>
+        <td data-testid={ `customer_checkout__element-order-table-name-${index}` }>
+          {title }
+        </td>
+        <td data-testid={ `customer_checkout__element-order-table-quantity-${index}` }>
+          { quantity }
+        </td>
+        <td data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }>
+          { price }
+        </td>
+        <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }>
+          { subTotal}
+        </td>
+        <td>
+          <button
+            type="button"
+            data-testid={ `customer_checkout__element-order-table-remove-${index}` }
+            onClick={ console.log('removido') }
+          >
+            Remover
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>;
 }
 
 TableRow.propTypes = {
