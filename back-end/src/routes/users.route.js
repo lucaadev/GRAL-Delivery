ec141b1fb6bb879d83c8bde7f5a87050e279fc85
Guilherme <guilhermehallmann@gmail.com>
2022-08-11 09:14:43 -0300

    fix: changing users route order

--- back-end/src/routes/users.route.js
+++ back-end/src/routes/users.route.js
@@ -3,9 +3,11 @@ const usersController = require('../controller/usersController');
 
 const users = express.Router();
 
+users.get('/all', usersController.getAllUsers);
+users.get('/:id', usersController.getUserById);
 users.get('/', usersController.getUsersRole);
 // users.get('search', usersController.getUserById);
-users.get('/:id', usersController.getUserById);
-users.get('/all', usersController.getAllUsers);
+
+
 
 module.exports = users;
