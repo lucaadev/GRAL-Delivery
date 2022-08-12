58541f2f938523e1114adf3274c28c5e87518639
Guilherme <guilhermehallmann@gmail.com>
2022-08-12 13:23:49 -0300

    fix: changing saleProduct query from findOne to findAll | feat: fetch saleProduct data in frontend

--- back-end/src/service/saleProductService.js
+++ back-end/src/service/saleProductService.js
@@ -1,12 +1,12 @@
 const { Sale, SaleProduct, Product } = require('../database/models');
 
 const getSaleProducts = async (id, key) => {
-  const saleProducts = await SaleProduct.findOne({
+  const saleProducts = await SaleProduct.findAll({
     where: { [key]: id },
-    include: [
-      { model: Product, as: 'product', through: { attributes: [] } },
-      { model: Sale, as: 'sale', through: { attributes: [] } },
-    ],
+    // include: [
+    //   { model: Product, as: 'product', through: { attributes: [] } },
+    //   { model: Sale, as: 'sale', through: { attributes: [] } },
+    // ],
   })
   return saleProducts;
 }
--- front-end/src/Pages/Order/orderDetails.js
+++ front-end/src/Pages/Order/orderDetails.js
@@ -1,19 +1,19 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/NavBar';
-import OrdersHeader from '../../components/OrdersHeader';
+// import OrdersHeader from '../../components/OrdersHeader';
 import Button from '../../components/Button';
 import axiosInstance from '../../utils/axios/axiosInstance';
 
 function Order() {
   const [order, setOrder] = useState([]);
+  const [products, setProducts] = useState([]);
   const { name } = JSON.parse(localStorage.getItem('user'));
   const { id } = useParams();
   const getOrder = useCallback(async () => {
     const { token } = JSON.parse(localStorage.getItem('user'));
     const config = {
       headers: { Authorization: token },
-      params: { id, key: 'id' },
     };
     try {
       const { data } = await axiosInstance
@@ -24,14 +24,33 @@ function Order() {
     }
   }, [id]);
 
-  useEffect(() => getOrder(), [getOrder]);
-  console.log(order);
+  const getOrderProducts = useCallback(async () => {
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    const config = {
+      headers: { Authorization: token },
+    };
+    try {
+      const { data } = await axiosInstance
+        .get(`/sp/${id}/sale_id`, config);
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    getOrder();
+    getOrderProducts();
+  }, [getOrder, getOrderProducts]);
+
+  console.log(products);
+
   const { totalPrice } = order;
 
   return (
     <section>
       <Header userName={ name } />
-      {
+      {/* {
         order.map((e) => (
           <OrdersHeader
             key={ e.id }
@@ -42,7 +61,7 @@ function Order() {
             orderStatus={ e.status }
           />
         ))
-      }
+      } */}
       <Button dataTestid="customer_order_details__element-order-total-price">
         Total R$:
         {' '}
