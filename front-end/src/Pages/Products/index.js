65ef256e07eef2ee8ce130b7e034032509a39acd
Guilherme <guilhermehallmann@gmail.com>
2022-08-17 10:13:34 -0300

    fix: changing productCard folder to Product

--- front-end/src/Pages/Products/index.js
+++ front-end/src/Pages/Products/index.js
@@ -2,7 +2,7 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CartBtn from '../../components/CartButton';
 import Header from '../../components/Header';
-import Card from '../../components/ProductCard';
+import Card from '../../components/Product/card';
 import axiosInstance from '../../utils/axios/axiosInstance';
 import DeliveryContext from '../../utils/context/DeliveryContext';
 
